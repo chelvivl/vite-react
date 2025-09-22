8d6072cf74c13511db4169f5fef944755d641614
chelvivl <vcheley15@mail.ru>
2025-09-22 11:05:17 +0300

    fixed ui2

--- src/App.tsx
+++ src/App.tsx
@@ -39,9 +39,11 @@ const handleContinueFromDay = (day: number) => {
 
   return (
     <>
-      <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
-        ⋯
-      </button>
+    <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+      </svg>
+    </button>
 
       <h1>📖 Библия за 111 дней</h1>
 
