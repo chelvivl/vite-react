806c4b81cb074d4a5e86f662c164924f0769ea86
chelvivl <vcheley15@mail.ru>
2025-09-25 08:30:32 +0300

    fixed

--- src/components/DayItem.tsx
+++ src/components/DayItem.tsx
@@ -127,6 +127,9 @@ return (
       height="24"
       viewBox="0 0 24 24"
       fill="none"
+      onClick={(_) => {
+        handleClick(dayIndex);
+      }}
       xmlns="http://www.w3.org/2000/svg"
       style={{ cursor: 'pointer' }}
     >
